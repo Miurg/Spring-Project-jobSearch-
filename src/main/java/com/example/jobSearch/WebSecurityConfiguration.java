743409bbc3400b0ead0886150d6e4bfc91fbc57e
Shimu <rashnimorgen@gmail.com>
2022-07-06 15:54:13 +0400

    fix websecurity (rework code from old version spring to new)

--- src/main/java/com/example/jobSearch/WebSecurityConfiguration.java
+++ src/main/java/com/example/jobSearch/WebSecurityConfiguration.java
@@ -1,55 +1,18 @@
 package com.example.jobSearch;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter; 
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    private String googleClientSecret;
-//
-//    @Autowired
-//    private CustomUserService customUserService;
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.springframework.security.config.annotation.web.configuration.
-     * WebSecurityConfigurerAdapter
-     * #configure(org.springframework.security.config
-     * .annotation.web.builders.HttpSecurity)
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        // @formatter:off
-        http
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/","/static/**", "/resources/**","/resources/public/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic().disable()
-                .requiresChannel().anyRequest().requiresSecure();
-        // @formatter:on
-        super.configure(http);
+public class WebSecurityConfiguration {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+        return (web) -> web.ignoring().antMatchers("/greeting", "/ignore2");
     }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth)
-//            throws Exception {
-//        // @formatter:off
-//        auth
-//                .eraseCredentials(true)
-//                .userDetailsService(customUserService);
-//        // @formatter:on
-//        super.configure(auth);
-//    }
 }
